83119ba03dcaf00882e7328e9e550bcec0a1a0cb
Arlikache <3241951559@qq.com>
2024-05-31 18:16:57 +0800

    5-31 unmeaningful push

--- test_func.py
+++ test_func.py
@@ -1,11 +1,27 @@
-import pandas as pd  
-import numpy as np  
-  
-ipl_data = {'Team': ['Riders', 'Riders', 'Devils', 'Devils', 'Kings',
-   'kings', 'Kings', 'Kings', 'Riders', 'Royals', 'Royals', 'Riders'],
-   'Rank': [1, 2, 2, 3, 3,4 ,1 ,1,2 , 4,1,2],
-   'Year': [2014,2015,2014,2015,2014,2015,2016,2017,2016,2014,2015,2017],
-   'Points':[876,789,863,673,741,812,756,788,694,701,804,690]}
-df = pd.DataFrame(ipl_data)
+from pypinyin import pinyin, Style
+import pandas 
 
-print()
+def chinese_to_pinyin(phrase):
+   # 返回汉字短语的每个字的首字母大写构成的字符串
+   pinyin_list = pinyin(phrase,style=Style.FIRST_LETTER, strict=False)
+   initials = ''.join(char[0].upper() for char in pinyin_list if char)
+   return initials
+
+
+if __name__ == '__main__':
+   dqbs_map = pandas.read_excel(r"D:\donghua\数据集市规则定义202404.xlsx",sheet_name="dqbs映射")
+   dqbs_phrase = pandas.read_excel(r"D:\donghua\数据集市规则定义202404.xlsx",sheet_name="dqbs字典")
+   dqbs_phrase.columns = ['字典值','中文描述','空']
+   dqbs_rs = pandas.DataFrame()
+   dqbs_rs['NAME_EN'] = dqbs_phrase.apply(
+         lambda row:'DQBS_' + chinese_to_pinyin(row['中文描述']) if row['字典值'] == '#' else None,
+         axis=1
+      )
+   dqbs_rs['NAME'] = dqbs_phrase.apply(
+         lambda row:row['中文描述'] if row['字典值'] == '#' else None,
+         axis=1
+      )
+   # dqbs_rs.dropna(subset=['NAME','NAME_EN'])
+   dqbs_rs = dqbs_rs[dqbs_rs['NAME'].notna()].reset_index(drop=True)
+   dqbs_rs.to_excel(r'D:\donghua\dqbs_rs.xlsx')
+   
