ff8daa1217edac1357ffab56b2cc3be478b160c9
Arlikache <3241951559@qq.com>
2024-07-25 17:40:05 +0800

    7-25 react - 2

--- learn/test_func.py
+++ learn/test_func.py
@@ -1,3 +1,42 @@
+import pandas as pd
+from dbfread import DBF
+
+
+path = r'D:\donghua\mission\7-24 底层资产相关资料\bond_info-8\PAR_BOND_INFO.dbf' # 文件目录
+table = DBF(path, encoding='GBK')
+df = pd.DataFrame(iter(table))
+
+# df2 = pd.DataFrame(iter(DBF(r'D:\donghua\mission\7-24 底层资产相关资料\bond_info-8\',encoding='GBK')))
+
+df_filter = df[df['ZQDM'].apply(
+    lambda x:True if x in ('1828009',
+        '1828012',
+        '1928036',
+        '1928021',
+        '1928018',
+        '1928028',
+        '1928011',
+        '1928009',
+        '1928004') 
+        else False)
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def test():
     """
     字符串令牌化：
@@ -15,21 +54,6 @@ def test():
 
     master_pat = re.compile('|'.join([NAME, NUM, PLUS, TIMES, EQ, WS]))
     scanner = master_pat.scanner('foo = 42')
-
-
-import pandas as pd
-from dbfread import DBF
-path = r'D:\Projects\Repo 3\task_files\20191212\000003_ZRTJSTZ.dbf' # 文件目录
-table = DBF(path, encoding='GBK')
-df = pd.DataFrame(iter(table))
-
-
-
-
-
-
-
-
 
 
 
